f4660c172f2dea2bf400604a3094ff46d1069c27
LegendAdamHembrough <adam.hembrough@l1.com>
2025-05-05 12:34:41 +0100

    Create Orders list collection class

---
+++ Entities/Collections/Orders.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Entities\Collections;
+
+use App\Entities\EntityCollection;
+use Entities\OrderEntity;
+
+class Orders extends EntityCollection
+{
+    /**
+     * Defines that this collection can only contain Order entities
+     *
+     * This class answers Programming question 5:
+     *  - it contains an array of OrderEntity classes
+     *  - the EntityCollection class has a public 'get()' method to return the list
+     *  - The EntityCollection class is also iterable so foreach is directly supported
+     */
+    protected function getCollectionEntityType(): string
+    {
+        return OrderEntity::class;
+    }
+}
